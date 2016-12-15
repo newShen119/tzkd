fdd1b3cd2053e90187817a76ded4ebc6dc1cccb4
gaoxiong <1056834607@qq.com>
2016-12-15 15:52:03 +0800

    测试C

--- 12-同舟注册/js/register.js
+++ 12-同舟注册/js/register.js
@@ -22,14 +22,14 @@ $(function(){
 
 	//手机验证规则  
 	jQuery.validator.addMethod("mobile", function (value, element) {
-	    var mobile = /^1[3|4|5|7|8]\d{9}$/;
+	   var mobile = /^1[3|4|5|7|8]\d{9}$/;
 		return this.optional(element) || (mobile.test(value));
 	}, "手机号码格式不对");
 
 
 	//邮箱或手机验证规则  
 	jQuery.validator.addMethod("emailOrMobile", function (value, element) {
-	    var mm = /^[a-z0-9._%-]+@([a-z0-9-]+\.)+[a-z]{2,4}$|^1[3|4|5|7|8]\d{9}$/;
+	   var mm = /^[a-z0-9._%-]+@([a-z0-9-]+\.)+[a-z]{2,4}$|^1[3|4|5|7|8]\d{9}$/;
 		return this.optional(element) || (mm.test(value));
 	}, "格式不对");
 
@@ -37,10 +37,8 @@ $(function(){
 	jQuery.validator.addMethod("idCard", function (value, element) {
 	    var isIDCard1=/^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$/;//(15位)
 	    var isIDCard2=/^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/;//(18位)
-
 	    return this.optional(element) || (isIDCard1.test(value)) || (isIDCard2.test(value));
 	}, "格式不对");
-
 
     $("form").eq(0).validate({
     	onblur: true, 
